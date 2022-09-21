0c19821c0aeb511258b5d1ecee1a2f33f1bef831
Jonas Ferreira <jonas.abreuferreira@gmail.com>
2022-09-21 09:44:14 -0300

    fix: improving schema builders

--- itidigital/data_quality/schema/builder.py
+++ itidigital/data_quality/schema/builder.py
@@ -5,32 +5,31 @@ from itidigital.data_quality.schema.event import EventSchema, SchemaField, Objec
 from itidigital.data_quality.event.event import FieldType
 
 
-# TODO: Consider change `get` methods to property
 class SchemaFieldBuilder(BaseBuilder):
     def __init__(self, config: dict) -> None:
-        self._raw_field = config
+        self._config = config
 
-    def get_id(self):
-        return self._raw_field.get('$id')
+    def get_id(self) -> str:
+        return self._config.get('$id')
 
-    def get_type(self):
+    def get_type(self) -> FieldType:
         try:
-            type_name = self._raw_field.get('type', '')
+            type_name = self._config.get('type', '')
             return FieldType[type_name.upper()]
 
         except KeyError:
             return FieldType.UNKNOWN
 
-    def get_title(self):
-        return self._raw_field.get('title', '')
+    def get_title(self) -> str:
+        return self._config.get('title', '')
 
-    def get_description(self):
-        return self._raw_field.get('description', '')
+    def get_description(self) -> str:
+        return self._config.get('description', '')
 
-    def get_examples(self):
-        return self._raw_field.get('examples', [])
+    def get_examples(self) -> List:
+        return self._config.get('examples', [])
 
-    def construct(self) -> object:
+    def construct(self) -> SchemaField:
         kwargs = {}
 
         for field in SchemaField.__dataclass_fields__:
@@ -42,69 +41,69 @@ class SchemaFieldBuilder(BaseBuilder):
 
 class ObjectFieldBuilder(BaseBuilder):
     def __init__(self, config: dict) -> None:
-        self._raw_object_field = config
+        self._config = config
 
-    def get_id(self):
-        return self._raw_object_field.get("$id")
+    def get_id(self) -> str:
+        return self._config.get("$id")
 
-    def get_type(self):
+    def get_type(self) -> FieldType:
         try:
-            type_name = self._raw_object_field.get('type', '')
+            type_name = self._config.get('type', '')
             return FieldType[type_name.upper()]
 
         except KeyError:
             return FieldType.UNKNOWN
 
-    def construct(self) -> object:
-        pass
-
-
-class SchemaBuilder(BaseBuilder):
-    def __init__(self, config: dict) -> None:
-        self._raw_schema = config
-
-    @property
-    def get_schema(self) -> str:
-        return self._raw_schema.get('$schema', '')
-
-    def get_id(self):
-        return self._raw_schema.get('$id', '')
-
-    def get_type(self) -> str:
-        return self._raw_schema.get('type', '')
-
     def get_title(self) -> str:
-        return self._raw_schema.get('title', '')
+        return self._config.get('title', '')
 
     def get_description(self) -> str:
-        return self._raw_schema.get('description', '')
+        return self._config.get('description', '')
 
     def get_required(self) -> List[str]:
-        return self._raw_schema.get('required', [])
+        return self._config.get('required', [])
 
-    @staticmethod
-    def get_field(raw_field: dict):
-        builder = SchemaFieldBuilder(
-            config=raw_field
-        )
+    def get_properties(self) -> List[SchemaField]:
+        properties = []
+        raw_properties: dict = self._config.get('properties')
 
-        return builder.construct()
+        if not raw_properties:
+            return properties
 
-    def get_properties(self):
-        fields = []
-        properties: dict = self._raw_schema.get('properties', {})
+        for name, value in raw_properties.items():
+            property_type_name = value.get('type', 'unknown')
+            property_type = FieldType[property_type_name.upper()]
 
-        if not properties:
-            return fields
+            if property_type == FieldType.OBJECT:
+                builder = ObjectFieldBuilder(config=value)
 
-        for name, value in properties.items():
-            field = self.get_field(raw_field=value)
-            fields.append(field)
+            else:
+                builder = SchemaFieldBuilder(config=value)
 
-        return fields
+            property = builder.construct()
+            properties.append(property)
+
+        return properties
+
+    def construct(self) -> ObjectField:
+        kwargs = {}
+
+        for field in ObjectField.__dataclass_fields__:
+            method = getattr(self, f'get_{field}')
+            kwargs[field] = method()
+
+        return ObjectField(**kwargs)
+
+
+class SchemaBuilder(ObjectFieldBuilder):
+    def __init__(self, config: dict) -> None:
+        super().__init__(config=config)
+        self._config = config
+
+    def get_schema(self) -> str:
+        return self._config.get('$schema', '')
 
     def construct(self) -> EventSchema:
-        # TODO: review error handling
         kwargs = {}
 
         for field in EventSchema.__dataclass_fields__:
@@ -112,20 +111,3 @@ class SchemaBuilder(BaseBuilder):
             kwargs[field] = method()
 
         return EventSchema(**kwargs)
-
-
-def main():
-    import json
-
-    file_path = '/home/jns/Documents/repos/itidigital-data-challenge/itidigital/data_quality/schema.json'
-    mode = 'r'
-
-    with open(file_path, mode) as schema_file:
-        raw_schema = json.loads(schema_file.read())
-
-    builder = SchemaBuilder(config=raw_schema)
-    schema = builder.construct()
-    for req_field in schema.required:
-        print(req_field)
-
-main()
