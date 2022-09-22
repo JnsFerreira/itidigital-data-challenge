d66aaaee0d38a12509d10165b5d511c7d0442a45
Jonas Ferreira <jonas.abreuferreira@gmail.com>
2022-09-22 11:09:00 -0300

    feat: data structures related to event schema

--- itidigital/data_quality/schema/event.py
+++ itidigital/data_quality/schema/event.py
@@ -1,30 +1,62 @@
 from typing import List, Any, Union
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class SchemaField:
-    id: str
-    type: str
-    title: str
-    description: str
-    examples: List[Any]
+    """
+    Class to represent the data structure of a schema field
+
+    Args:
+        id (str): Schema field id
+        name (str): Schema field name
+        type (str): Schema field type
+        title (str): Schema field type
+        description (str): Schema field description
+        examples (List[Any]): Examples of possible values
+    """
+    id: str = field(compare=False)
+    name: str = field(compare=True)
+    type: str = field(compare=True)
+    title: str = field(compare=False)
+    description: str = field(compare=False)
+    examples: List[Any] = field(compare=False)
 
 
 @dataclass
 class ObjectField:
-    id: str
-    type: str
-    title: str
-    description: str
-    required: List[str]
-    properties: List[SchemaField]
+    """
+    Class to represent the data structure of an object field
+
+    Args:
+        id (str): Object field id
+        name (str): Object field name
+        type (str): Object field type
+        title (str): Object field type
+        description (str): Object field description
+        required (List[str]): Object field required fields
+        properties (List[SchemaField]): List with all object properties
+    """
+    id: str = field(compare=False)
+    name: str = field(compare=True)
+    type: str = field(compare=True)
+    title: str = field(compare=False)
+    description: str = field(compare=False)
+    required: List[str] = field(compare=False)
+    properties: List[SchemaField] = field(compare=True)
 
 
 @dataclass
 class EventSchema(ObjectField):
-    schema: str
-    properties: List[Union[SchemaField, ObjectField]]
+    """
+    Class to represent the data structure of an event schema
+
+    Args:
+        schema (str): schema name
+        properties (List[Union[SchemaField, ObjectField]]): List with all schema properties
+    """
+    schema: str = field(compare=False)
+    properties: List[Union[SchemaField, ObjectField]] = field(compare=True)
 
 
 
