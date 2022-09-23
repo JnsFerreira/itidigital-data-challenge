55cfe3c4275e3744bce13c98185241339dbe354e
Jonas Ferreira <jonas.abreuferreira@gmail.com>
2022-09-23 17:54:30 -0300

    feat: adding invalid row format exception

--- itidigital/sql/athena/exceptions.py
+++ itidigital/sql/athena/exceptions.py
@@ -2,3 +2,9 @@ class InvalidS3LocationError(Exception):
     """Exception for invalid S3 locations"""
     def __init__(self, message: str) -> None:
         super().__init__(message)
+
+
+class InvalidRowFormatError(Exception):
+    """Exception for invalid row formats"""
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
